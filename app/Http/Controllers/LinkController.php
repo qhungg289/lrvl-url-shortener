2ce16e134dd4b4955e5a207dd38dbbbb086658ca
Hung Duong Quang <qhungg289@protonmail.com>
2022-07-11 20:36:09 +0700

    Init commit

---
+++ app/Http/Controllers/LinkController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LinkController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function create()
+    {
+    }
+
+    public function store()
+    {
+    }
+
+    public function redirect()
+    {
+    }
+}
