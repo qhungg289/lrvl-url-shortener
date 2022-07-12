6ab778e49fa6ed738de7a45962de0a888dca23d0
Hung Duong Quang <qhungg289@protonmail.com>
2022-07-12 19:52:46 +0700

    Implement LinkController

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -2,24 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Link;
+use Tuupola\Base62;
 use Illuminate\Http\Request;
 
 class LinkController extends Controller
 {
-    public function index()
-    {
-        return view('index');
-    }
-
     public function create()
     {
+        return view('link.create');
     }
 
-    public function store()
+    public function store(Request $request, Base62 $base62)
     {
+        $validated = $request->validate([
+            'link' => 'required|url'
+        ]);
+
+        $link = Link::create([
+            'destination' => $validated['link']
+        ]);
+
+        $link->short_code = $base62->encode($link->id);
+        $link->save();
+
+        return view('link.create', ['short_link' => url($link->short_code), 'full_link' => $link->destination]);
     }
 
-    public function redirect()
+    public function redirect($code)
     {
+        $link = Link::where('short_code', $code)->first();
+
+        if (empty($link)) {
+            return abort(404);
+        }
+
+        $link->visits_count += 1;
+        $link->save();
+
+        return redirect()->away($link->destination);
     }
 }
