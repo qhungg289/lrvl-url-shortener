6c82e9486281305d2412c67537cfd1eb45cb57c8
Hung Duong Quang <qhungg289@protonmail.com>
2022-07-13 21:13:33 +0700

    Update web routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\LinkController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,8 +20,20 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/{code}', [LinkController::class, 'redirect']);
+Route::controller(AuthController::class)->group(function () {
+    Route::get('/signup', 'get_signup')->name('signup')->middleware('guest');
+    Route::post('/signup', 'post_signup');
 
-Route::get('/link/create', [LinkController::class, 'create']);
+    Route::get('/login', 'get_login')->name('login')->middleware('guest');
+    Route::post('/login', 'post_login');
 
-Route::post('/link/create', [LinkController::class, 'store']);
+    Route::post('/logout', 'logout');
+});
+
+Route::get('/profile', [UserController::class, 'profile'])->name('profile')->middleware('auth');
+
+Route::controller(LinkController::class)->group(function () {
+    Route::get('/link', 'create');
+    Route::post('/link', 'store');
+    Route::get('/{code}', 'redirect');
+});
