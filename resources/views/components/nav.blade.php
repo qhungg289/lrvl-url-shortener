f58d04882d57b02117833cfdd103dc06aa6c6597
Hung Duong Quang <qhungg289@protonmail.com>
2022-12-17 23:05:43 +0700

    Update nav.blade.php

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -5,21 +5,33 @@
         <div class="flex gap-2 relative" x-data="{ open: false }">
             @auth
                 <button @click="open = !open"
-                    class="font-semibold border border-slate-700 flex items-center gap-1 px-5 py-2 rounded-md focus:bg-slate-800 hover:bg-slate-800 transition-colors">
-                    <span>{{ auth()->user()->name }}</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd"
-                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                            clip-rule="evenodd" />
+                    class="border border-slate-700 flex items-center gap-1 px-5 py-2 rounded-md focus:bg-slate-800 hover:bg-slate-800 transition-colors">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
                     </svg>
                 </button>
 
                 <div x-show="open" x-cloak x-transition @click.outside="open = false"
-                    class="rounded-md border border-slate-700 bg-slate-800 absolute top-11 right-0 p-4 space-y-4 shadow">
-                    <a href="/profile">Profile</a>
+                    class="rounded-md border border-slate-700 bg-slate-800 absolute min-w-max top-11 right-0 shadow">
+                    <a href="/profile"
+                        class="hover:underline block px-4 py-3 m-4 rounded-md hover:bg-slate-600 shadow-2xl transition-colors">
+                        <div class="flex items-center justify-center gap-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                class="w-14 h-14">
+                                <path fill-rule="evenodd"
+                                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                            <span>{{ auth()->user()->name }}</span>
+                        </div>
+                    </a>
                     <form action="/logout" method="post">
                         @csrf
-                        <button type="submit">Log out</button>
+                        <button type="submit" class="hover:underline w-full px-4 py-3 text-slate-400">
+                            Log out
+                        </button>
                     </form>
                 </div>
             @endauth
