3b92c416e141d09c11951234ca978087e5590181
Hung Duong Quang <qhungg289@protonmail.com>
2022-12-17 23:05:14 +0700

    Update favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,6 +9,7 @@
 
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800;900&display=swap"
         rel="stylesheet">
+    <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon">
 
     @vite('resources/css/app.css')
     <style>
