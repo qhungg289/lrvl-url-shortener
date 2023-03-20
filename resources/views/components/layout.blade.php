03162abceaf543bc37da9d28358e5f9ad33194a4
Hung Duong Quang <qhungg289@protonmail.com>
2023-03-20 19:57:52 +0700

    change favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,8 +9,10 @@
 
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800;900&display=swap"
         rel="stylesheet">
-    <link rel="icon"
-        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍒</text></svg>">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
